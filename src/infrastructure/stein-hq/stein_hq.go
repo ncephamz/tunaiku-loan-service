1724b7b4721e3febc7f8600105468dc211c43be7
Encep Hamzah F R <ncephamz@Enceps-MacBook-Pro.local>
2022-08-16 17:52:05 +0700

    remove username and password

--- src/infrastructure/stein-hq/stein_hq.go
+++ src/infrastructure/stein-hq/stein_hq.go
@@ -10,9 +10,7 @@ func NewSteinHqConnection() *stein.Stein {
 	return stein.NewClient(
 		"https://api.steinhq.com/v1/storages/62f5ad10bca21f053ea7ff39",
 		&stein.Option{
-			Username: "ncephamz@gmail.com",
-			Password: "26feb1994",
-			Timeout:  20 * time.Second,
+			Timeout: 20 * time.Second,
 		},
 	)
 }
